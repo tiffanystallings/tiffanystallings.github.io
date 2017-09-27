595cc8a1a8517c641c666e450e1d67842c79e954
Tiffany <tiffany.c.stallings@gmail.com>
2017-09-26 23:18:26 -0400

    Re-do contact form to use email.js api

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$('#send').submit(function(e) {
+	$('.flash').text("");
+	var name = $('#name').val();
+	var email = $('#email').val();
+	var text = $('#text').val();
+
+	if (name && email && text) {
+		console.log('Conditions met.');
+		emailjs.send("gmail", "standard", 
+			{
+				"name": name, 
+				"email": email,
+				"message": text
+			}).then(function(response) {
+				console.log(response.status + response.text);
+				$('.flash').text("Message sent!");
+			}, function(err) {
+				console.log(err);
+				$('.flash').text("Unable to send message. Try again later!");
+			});
+		$('#name').val("");
+		$('#email').val("");
+		$('#text').val("");
+	} else {
+		$('.flash').text("All fields required.");
+	}
+	e.preventDefault();
+})
